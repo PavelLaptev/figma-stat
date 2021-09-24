6fa5d8d004254085be8854a4347af6f41e599be6
LaptevPavel <laptev.graphics@gmail.com>
2021-09-24 12:09:24 +0200

    Merge branch 'main' of https://github.com/PavelLaptev/figma-stat into main

--- scripts/utilities/fetchPluginsDataAsync.js
+++ scripts/utilities/fetchPluginsDataAsync.js
@@ -6,18 +6,18 @@ export async function fetchPluginsDataAsync() {
   let url =
     'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=50';
 
-  // while (typeof url !== 'undefined') {
-  //   const response = await fetchAsync(url);
-  //   const json = await response.json();
-  //   result = result.concat(parseRawPluginsData(json.meta.plugins));
-  //   url = json.pagination.next_page;
-  // }
+  while (typeof url !== 'undefined') {
+    const response = await fetchAsync(url);
+    const json = await response.json();
+    result = result.concat(parseRawPluginsData(json.meta.plugins));
+    url = json.pagination.next_page;
+  }
 
-  const response = await fetchAsync(url);
-  const json = await response.json();
+  // const response = await fetchAsync(url);
+  // const json = await response.json();
   // result = result.concat(json.meta.plugins);
 
-  result = parseRawPluginsData(json.meta.plugins);
+  // result = parseRawPluginsData(json.meta.plugins);
 
   // console.log(json.meta.plugins);
   // console.log(result);
