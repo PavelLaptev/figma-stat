74d577d944db20a23d5c96d3ba6f4b8a6db402a1
LaptevPavel <laptev.graphics@gmail.com>
2021-09-27 12:29:24 +0200

    updated hub fetch

--- scripts/utilities/fetchHubfilesDataAsync.js
+++ scripts/utilities/fetchHubfilesDataAsync.js
@@ -5,7 +5,7 @@ import { fetchAsync } from './fetchAsync.js';
 export async function fetchHubfilesDataAsync() {
   let result = [];
   let url =
-    'https://www.figma.com/api/hub_files/all?sort_by=popular&sort_order=desc&pagination_direction=next&page_size=25';
+    'https://www.figma.com/api/hub_files/all?sort_by=&sort_order=desc&pagination_direction=next&page_size=25';
 
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
