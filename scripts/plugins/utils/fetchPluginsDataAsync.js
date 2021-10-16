e168c320811ac2688defc0e37892c2d61ebf2a50
LaptevPavel <laptev.graphics@gmail.com>
2021-10-16 10:58:35 +0200

    wip

--- scripts/plugins/utils/fetchPluginsDataAsync.js
+++ scripts/plugins/utils/fetchPluginsDataAsync.js
@@ -21,7 +21,7 @@ import { fetchAsync } from '../../utils/fetchAsync.js';
 
 export async function fetchPluginsDataAsync() {
   let url =
-    'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=5';
+    'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=6';
 
   const response = await fetchAsync(url);
   const json = await response.json();
--- scripts/plugins/utils/writePluginData.js
+++ scripts/plugins/utils/writePluginData.js
@@ -8,12 +8,22 @@ export async function writePluginData(data) {
   await data.forEach(async (plugin) => {
     const pluginUrl = `https://raw.githubusercontent.com/PavelLaptev/figma-stat/empty-branch/plugins/${plugin.info.id}/counters.json`;
 
-    await fetchAsync(pluginUrl).then((data) => {
-      if (data.status === 404) {
-        const directory = `data/plugins/${plugin.info.id}`;
-        const infoDir = `${directory}/info.json`;
-        const dateDir = `${directory}/counters.json`;
-        const currentDateDir = `${directory}/latest.json`;
+    const directory = `data/plugins/${plugin.info.id}`;
+    const infoDir = `${directory}/info.json`;
+    const dateDir = `${directory}/counters.json`;
+    const currentDateDir = `${directory}/latest.json`;
+
+    await fetchAsync(pluginUrl).then(async (data) => {
+      try {
+        const oldJSON = await data.json();
+        const newJSON = oldJSON.concat(plugin.counters);
+
+        fse.outputJsonSync(infoDir, plugin.info);
+        fse.outputJsonSync(dateDir, newJSON);
+        fse.outputJsonSync(currentDateDir, plugin.counters);
+      } catch (err) {
+        console.error(err);
+        console.log('CREATE NEW FOLDER');
 
         fse.outputJsonSync(infoDir, plugin.info);
         fse.outputJsonSync(dateDir, [plugin.counters]);
