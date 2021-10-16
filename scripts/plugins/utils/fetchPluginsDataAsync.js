e7792c870931cef4aacd4cd5dd347022c605870d
LaptevPavel <laptev.graphics@gmail.com>
2021-10-16 11:01:27 +0200

    wip

--- scripts/plugins/utils/fetchPluginsDataAsync.js
+++ scripts/plugins/utils/fetchPluginsDataAsync.js
@@ -2,30 +2,30 @@ import { writePluginData } from './writePluginData.js';
 import { parseRawPluginsData } from './parseRawPluginsData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
 
-// export async function fetchPluginsDataAsync() {
-//   let url =
-//     'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=25';
-//   let result = [];
-//   let count = 1;
-
-//   while (typeof url !== 'undefined') {
-//     const response = await fetchAsync(url);
-//     const json = await response.json();
-
-//     result = parseRawPluginsData(json.meta.plugins);
-//     url = json.pagination.next_page;
-//     console.log(count++, url);
-//     writePluginData(result);
-//   }
-// }
-
 export async function fetchPluginsDataAsync() {
   let url =
-    'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=6';
+    'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=25';
+  let result = [];
+  let count = 1;
 
-  const response = await fetchAsync(url);
-  const json = await response.json();
+  while (typeof url !== 'undefined') {
+    const response = await fetchAsync(url);
+    const json = await response.json();
 
-  let result = parseRawPluginsData(json.meta.plugins);
-  writePluginData(result);
+    result = parseRawPluginsData(json.meta.plugins);
+    url = json.pagination.next_page;
+    console.log(count++, url);
+    writePluginData(result);
+  }
 }
+
+// export async function fetchPluginsDataAsync() {
+//   let url =
+//     'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=6';
+
+//   const response = await fetchAsync(url);
+//   const json = await response.json();
+
+//   let result = parseRawPluginsData(json.meta.plugins);
+//   writePluginData(result);
+// }
