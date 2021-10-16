fd3e93dcebb454420d3025f211d94de24b143f8e
LaptevPavel <laptev.graphics@gmail.com>
2021-10-16 10:45:50 +0200

    wip

--- scripts/plugins/utils/writePluginData.js
+++ scripts/plugins/utils/writePluginData.js
@@ -1,15 +1,15 @@
 import fse from 'fs-extra';
-import fetch from 'node-fetch';
 
 import { fetchAsync } from '../../utils/fetchAsync.js';
 
 export async function writePluginData(data) {
   // const date = new Date().toISOString().slice(0, 10);
 
-  await data.forEach((plugin) => {
-    const pluginUrl = `https://raw.githubusercontent.com/PavelLaptev/figma-stat/gh-pages/${plugin.info.id}/counters.json`;
-    fetch(pluginUrl).then((response) => {
-      if (response.status === 404) {
+  await data.forEach(async (plugin) => {
+    const pluginUrl = `https://raw.githubusercontent.com/PavelLaptev/figma-stat/empty-branch/plugins/${plugin.info.id}/counters.json`;
+
+    await fetchAsync(pluginUrl).then((data) => {
+      if (data.status === 404) {
         const directory = `data/plugins/${plugin.info.id}`;
         const infoDir = `${directory}/info.json`;
         const dateDir = `${directory}/counters.json`;
