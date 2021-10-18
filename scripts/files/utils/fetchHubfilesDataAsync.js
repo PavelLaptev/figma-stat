eaee19244a59ba371be831820b5f357e4f9408e0
LaptevPavel <laptev.graphics@gmail.com>
2021-10-18 10:18:01 +0200

    Update fetchHubfilesDataAsync.js

--- scripts/files/utils/fetchHubfilesDataAsync.js
+++ scripts/files/utils/fetchHubfilesDataAsync.js
@@ -3,15 +3,15 @@ import { writeHubfilesData } from './writeHubfilesData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
 
 export async function fetchHubfilesDataAsync() {
-  let result = [];
   let url =
     'https://www.figma.com/api/hub_files/all?sort_by=created_at&sort_order=desc&pagination_direction=next&page_size=25';
+  let count = 1;
 
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
     const json = await response.json();
 
-    let result = parseRawHubfilesData(json.meta.hub_files);
+    const result = parseRawHubfilesData(json.meta.hub_files);
     url = json.pagination.next_page;
     console.log(count++, url);
     writeHubfilesData(result);
