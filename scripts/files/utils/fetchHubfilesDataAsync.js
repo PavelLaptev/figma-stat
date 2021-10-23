741748de89dfe5b8efc74914aa88b73dae48a9e5
LaptevPavel <laptev.graphics@gmail.com>
2021-10-23 03:18:37 +0200

    added totals jSON files

--- scripts/files/utils/fetchHubfilesDataAsync.js
+++ scripts/files/utils/fetchHubfilesDataAsync.js
@@ -1,3 +1,4 @@
+import fse from 'fs-extra';
 import { parseRawHubfilesData } from './parseRawHubfilesData.js';
 import { writeHubfilesData } from './writeHubfilesData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
@@ -5,7 +6,14 @@ import { fetchAsync } from '../../utils/fetchAsync.js';
 export async function fetchHubfilesDataAsync() {
   let url =
     'https://www.figma.com/api/hub_files/all?sort_by=created_at&sort_order=desc&pagination_direction=next&page_size=25';
-  let count = 1;
+
+  let totalCounts = {
+    files: 0,
+    duplicateCount: 0,
+    likeCount: 0,
+    viewCount: 0,
+    commentCount: 0,
+  };
 
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
@@ -13,9 +21,33 @@ export async function fetchHubfilesDataAsync() {
 
     const result = parseRawHubfilesData(json.meta.hub_files);
     url = json.pagination.next_page;
-    console.log(count++, url);
     writeHubfilesData(result);
+
+    totalCounts = {
+      files: totalCounts.files + result.length,
+      duplicateCount:
+        totalCounts.duplicateCount +
+        result
+          .map((file) => file.counters.duplicateCount)
+          .reduce((a, b) => a + b),
+      likeCount:
+        totalCounts.likeCount +
+        result.map((file) => file.counters.likeCount).reduce((a, b) => a + b),
+      viewCount:
+        totalCounts.viewCount +
+        result.map((file) => file.counters.viewCount).reduce((a, b) => a + b),
+      commentCount:
+        totalCounts.commentCount +
+        result
+          .map((file) => file.counters.commentCount)
+          .reduce((a, b) => a + b),
+    };
+
+    // console.log(totalCounts);
   }
+
+  console.log('done', totalCounts);
+  fse.outputJsonSync(`data/hub_files/total.json`, totalCounts);
 }
 
 // export async function fetchHubfilesDataAsync() {
--- scripts/plugins/utils/fetchPluginsDataAsync.js
+++ scripts/plugins/utils/fetchPluginsDataAsync.js
@@ -1,3 +1,4 @@
+import fse from 'fs-extra';
 import { writePluginData } from './writePluginData.js';
 import { parseRawPluginsData } from './parseRawPluginsData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
@@ -5,7 +6,14 @@ import { fetchAsync } from '../../utils/fetchAsync.js';
 export async function fetchPluginsDataAsync() {
   let url =
     'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=25';
-  let count = 1;
+
+  let totalCounts = {
+    plugins: 0,
+    installCount: 0,
+    likeCount: 0,
+    viewCount: 0,
+    commentCount: 0,
+  };
 
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
@@ -13,9 +21,37 @@ export async function fetchPluginsDataAsync() {
 
     let result = parseRawPluginsData(json.meta.plugins);
     url = json.pagination.next_page;
-    console.log(count++, url);
     writePluginData(result);
+
+    totalCounts = {
+      plugins: totalCounts.plugins + result.length,
+      installCount:
+        totalCounts.installCount +
+        result
+          .map((plugin) => plugin.counters.installCount)
+          .reduce((a, b) => a + b),
+      likeCount:
+        totalCounts.likeCount +
+        result
+          .map((plugin) => plugin.counters.likeCount)
+          .reduce((a, b) => a + b),
+      viewCount:
+        totalCounts.viewCount +
+        result
+          .map((plugin) => plugin.counters.viewCount)
+          .reduce((a, b) => a + b),
+      commentCount:
+        totalCounts.commentCount +
+        result
+          .map((plugin) => plugin.counters.commentCount)
+          .reduce((a, b) => a + b),
+    };
+
+    console.log(totalCounts);
   }
+
+  console.log('done', totalCounts);
+  fse.outputJsonSync(`data/plugins/total.json`, totalCounts);
 }
 
 // export async function fetchPluginsDataAsync() {
