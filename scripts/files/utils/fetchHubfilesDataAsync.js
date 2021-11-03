e215188195a8dc89a21cab897163b78bcae7bac8
LaptevPavel <laptev.graphics@gmail.com>
2021-11-03 10:30:40 +0100

    removed total counter

--- scripts/files/utils/fetchHubfilesDataAsync.js
+++ scripts/files/utils/fetchHubfilesDataAsync.js
@@ -1,4 +1,3 @@
-import fse from 'fs-extra';
 import { parseRawHubfilesData } from './parseRawHubfilesData.js';
 import { writeHubfilesData } from './writeHubfilesData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
@@ -7,14 +6,6 @@ export async function fetchHubfilesDataAsync() {
   let url =
     'https://www.figma.com/api/hub_files/all?sort_by=created_at&sort_order=desc&pagination_direction=next&page_size=25';
 
-  let totalCounts = {
-    files: 0,
-    duplicateCount: 0,
-    likeCount: 0,
-    viewCount: 0,
-    commentCount: 0,
-  };
-
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
     const json = await response.json();
@@ -22,33 +13,10 @@ export async function fetchHubfilesDataAsync() {
     const result = parseRawHubfilesData(json.meta.hub_files);
     url = json.pagination.next_page;
     writeHubfilesData(result);
-
-    totalCounts = {
-      files: totalCounts.files + result.length,
-      duplicateCount:
-        totalCounts.duplicateCount +
-        result
-          .map((file) => file.counters.duplicateCount)
-          .reduce((a, b) => a + b),
-      likeCount:
-        totalCounts.likeCount +
-        result.map((file) => file.counters.likeCount).reduce((a, b) => a + b),
-      viewCount:
-        totalCounts.viewCount +
-        result.map((file) => file.counters.viewCount).reduce((a, b) => a + b),
-      commentCount:
-        totalCounts.commentCount +
-        result
-          .map((file) => file.counters.commentCount)
-          .reduce((a, b) => a + b),
-    };
-
-    // console.log(totalCounts);
   }
-
-  console.log('done', totalCounts);
-  fse.outputJsonSync(`data/hub_files/total.json`, totalCounts);
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 // export async function fetchHubfilesDataAsync() {
 //   let url =
@@ -61,3 +29,5 @@ export async function fetchHubfilesDataAsync() {
 
 //   writeHubfilesData(result);
 // }
+
+////////////////////////////////////////////////////////////////////////////////
--- scripts/plugins/utils/fetchPluginsDataAsync.js
+++ scripts/plugins/utils/fetchPluginsDataAsync.js
@@ -1,4 +1,3 @@
-import fse from 'fs-extra';
 import { writePluginData } from './writePluginData.js';
 import { parseRawPluginsData } from './parseRawPluginsData.js';
 import { fetchAsync } from '../../utils/fetchAsync.js';
@@ -7,14 +6,6 @@ export async function fetchPluginsDataAsync() {
   let url =
     'https://www.figma.com/api/plugins/browse?sort_by=popular&sort_order=desc&resource_type=plugins&page_size=25';
 
-  let totalCounts = {
-    plugins: 0,
-    installCount: 0,
-    likeCount: 0,
-    viewCount: 0,
-    commentCount: 0,
-  };
-
   while (typeof url !== 'undefined') {
     const response = await fetchAsync(url);
     const json = await response.json();
@@ -22,37 +13,10 @@ export async function fetchPluginsDataAsync() {
     let result = parseRawPluginsData(json.meta.plugins);
     url = json.pagination.next_page;
     writePluginData(result);
-
-    totalCounts = {
-      plugins: totalCounts.plugins + result.length,
-      installCount:
-        totalCounts.installCount +
-        result
-          .map((plugin) => plugin.counters.installCount)
-          .reduce((a, b) => a + b),
-      likeCount:
-        totalCounts.likeCount +
-        result
-          .map((plugin) => plugin.counters.likeCount)
-          .reduce((a, b) => a + b),
-      viewCount:
-        totalCounts.viewCount +
-        result
-          .map((plugin) => plugin.counters.viewCount)
-          .reduce((a, b) => a + b),
-      commentCount:
-        totalCounts.commentCount +
-        result
-          .map((plugin) => plugin.counters.commentCount)
-          .reduce((a, b) => a + b),
-    };
-
-    console.log(totalCounts);
   }
-
-  console.log('done', totalCounts);
-  fse.outputJsonSync(`data/plugins/total.json`, totalCounts);
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 // export async function fetchPluginsDataAsync() {
 //   let url =
@@ -65,3 +29,5 @@ export async function fetchPluginsDataAsync() {
 
 //   writePluginData(result);
 // }
+
+////////////////////////////////////////////////////////////////////////////////
